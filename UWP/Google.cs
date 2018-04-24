110f4603a6cd832baf9a363fa734961a7c0fe2cc
ms.zojaji <ms.zojaji@gmail.com>
2018-04-24 20:26:49 +0430

    Fix Signin issue of UWP

--- UWP/Google.cs
+++ UWP/Google.cs
@@ -1,45 +1,186 @@
 ﻿namespace Zebble
 {
     using System;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text;
     using System.Threading.Tasks;
-    using Windows.ApplicationModel.Core;
-    using Windows.UI.Core;
-    using Windows.UI.ViewManagement;
+    using Windows.Data.Json;
+    using Windows.Security.Cryptography;
+    using Windows.Security.Cryptography.Core;
+    using Windows.Storage;
+    using Windows.Storage.Streams;
 
     public partial class Google
     {
-        const string AUTH_URL = "https://accounts.google.com/o/oauth2/auth";
-        const string ACCESS_TOKEN = "https://accounts.google.com/o/oauth2/token";
+        const string REDIRECT_URI = "pw.oauth2:/oauth2redirect";
+        const string AUTH_END_POINT = "https://accounts.google.com/o/oauth2/v2/auth";
+        const string TOKEN_END_POINT = "https://www.googleapis.com/oauth2/v4/token";
+        const string USER_INFO_END_POINT = "https://www.googleapis.com/oauth2/v3/userinfo";
 
-        public static Task SignIn(string clientId, string clientSecret)
+        static string ClientId;
+
+        public static async Task SignIn(string clientId, string clientSecret)
         {
-            return Thread.UI.Run(async () =>
+            try
             {
-                var auth = new Xamarin.Auth.OAuth2Authenticator(clientId, clientSecret, "openid email profile", new Uri(AUTH_URL),
-                    new Uri("urn:ietf:wg:oauth:2.0:oob:auto"), new Uri(ACCESS_TOKEN), null, true)
-                { AllowCancel = true };
+                ClientId = clientId;
 
-                auth.Completed += (sender, ev) =>
+                string state = RandomDataBase64Url(32);
+                string codeVerifier = RandomDataBase64Url(32);
+                string codeChallenge = Base64UrlEncodeNoPadding(Sha256(codeVerifier));
+                const string codeChallengeMethod = "S256";
+
+                var localSettings = ApplicationData.Current.LocalSettings;
+                localSettings.Values["state"] = state;
+                localSettings.Values["code_verifier"] = codeVerifier;
+
+                var authorizationRequest =
+                    string.Format("{0}?response_type=code&scope=openid%20profile&redirect_uri={1}&client_id={2}&state={3}&code_challenge={4}&code_challenge_method={5}",
+                    AUTH_END_POINT,
+                    Uri.EscapeDataString(REDIRECT_URI),
+                    clientId,
+                    state,
+                    codeChallenge,
+                    codeChallengeMethod);
+
+                await Nav.ShowPopUp<GoogleUI>(new { Url = authorizationRequest });
+            }
+            catch (Exception ex)
+            {
+                Device.Log.Error(ex.Message);
+            }
+        }
+
+        internal static string RandomDataBase64Url(uint length)
+        {
+            var buffer = CryptographicBuffer.GenerateRandom(length);
+            return Base64UrlEncodeNoPadding(buffer);
+        }
+
+        internal static IBuffer Sha256(string inputStirng)
+        {
+            var sha = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha256);
+            var buff = CryptographicBuffer.ConvertStringToBinary(inputStirng, BinaryStringEncoding.Utf8);
+            return sha.HashData(buff);
+        }
+
+        internal static string Base64UrlEncodeNoPadding(IBuffer buffer)
+        {
+            string base64 = CryptographicBuffer.EncodeToBase64String(buffer);
+
+            // Converts base64 to base64url.
+            base64 = base64.Replace("+", "-");
+            base64 = base64.Replace("/", "_");
+            // Strips padding.
+            base64 = base64.Replace("=", "");
+
+            return base64;
+        }
+
+        internal class GoogleUI : Page
+        {
+            public override async Task OnInitializing()
+            {
+                await base.OnInitializing();
+
+                this.Width(Root.ActualWidth - 50).Height(Root.ActualHeight - 70).Margin(top: 20).Background(color: Colors.White);
+
+                var header = new Stack { Direction = RepeatDirection.Horizontal };
+
+                var title = new TextView { Text = "GoogleSignIn" };
+                title.Padding(all: 5).Border(bottom: 1, color: Colors.Silver).Width(100.Percent());
+
+                var closeBtn = new Button { Text = "x" };
+                closeBtn.TextColor(Colors.Gray).Width(20).Height(20);
+                closeBtn.Tapped.Handle(() => Nav.HidePopUp());
+
+                await header.Add(title);
+                await header.Add(closeBtn);
+
+                var authorizationRequest = Nav.Param<string>("Url");
+                var browser = new WebView(authorizationRequest);
+                browser.Width(100.Percent()).Height(100.Percent()).Margin(top: title.ActualHeight);
+                browser.BrowserNavigating.Handle(args => OnNavigating(args));
+
+                header.Height.BindTo(title.Height);
+
+                await Add(header);
+                await Add(browser);
+            }
+
+            public async Task OnNavigating(WebView.NavigatingEventArgs args)
+            {
+                if (args.Url != null && args.Url != "")
                 {
-                    if (ev.IsAuthenticated)
-                        UserSignedIn.Raise(ev.Account);
-                };
+                    // Gets URI from navigation parameters.
+                    var authorizationResponse = new Uri(args.Url);
+                    string queryString = authorizationResponse.Query;
 
-                var ui = auth.GetUI();
-                var newView = CoreApplication.CreateNewView();
-                int newViewId = 0;
-                await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    var queryStringParams = queryString.Substring(1).Split('&')
+                        .ToDictionary(c => c.Split('=')[0], c => Uri.UnescapeDataString(c.Split('=')[1]));
+
+                    if (queryStringParams.ContainsKey("error"))
+                    {
+                        await Nav.HidePopUp();
+                        return;
+                    }
+
+                    if (!queryStringParams.ContainsKey("code") || !queryStringParams.ContainsKey("state"))
+                        return;
+
+                    string code = queryStringParams["code"];
+                    string incomingState = queryStringParams["state"];
+
+                    var localSettings = ApplicationData.Current.LocalSettings;
+                    var expectedState = (string)localSettings.Values["state"];
+
+                    if (incomingState != expectedState)
+                        return;
+
+                    localSettings.Values["state"] = null;
+
+                    string codeVerifier = (string)localSettings.Values["code_verifier"];
+                    await PerformCodeExchangeAsync(code, codeVerifier);
+                }
+                else
                 {
-                    var frame = new Windows.UI.Xaml.Controls.Frame();
-                    frame.Navigate(ui, auth);
-                    Windows.UI.Xaml.Window.Current.Content = frame;
-                    // You have to activate the window in order to show it later.
-                    Windows.UI.Xaml.Window.Current.Activate();
+                    Device.Log.Message(args.Url);
 
-                    newViewId = ApplicationView.GetForCurrentView().Id;
-                });
-                bool viewShown = await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
-            });
+                    await Nav.HidePopUp();
+                    await Task.CompletedTask;
+                }
+            }
+
+            async Task PerformCodeExchangeAsync(string code, string codeVerifier)
+            {
+                // Builds the Token request
+                string tokenRequestBody = string.Format("code={0}&redirect_uri={1}&client_id={2}&code_verifier={3}&scope=&grant_type=authorization_code",
+                    code,
+                    Uri.EscapeDataString(REDIRECT_URI),
+                    ClientId,
+                    codeVerifier
+                    );
+
+                var content = new StringContent(tokenRequestBody, Encoding.UTF8, "application/x-www-form-urlencoded");
+                var handler = new HttpClientHandler { AllowAutoRedirect = true };
+                var client = new HttpClient(handler);
+                var response = await client.PostAsync(TOKEN_END_POINT, content);
+                string responseString = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                    return;
+
+                var tokens = JsonObject.Parse(responseString);
+                string accessToken = tokens.GetNamedString("access_token");
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+                var userinfoResponse = await client.GetAsync(USER_INFO_END_POINT);
+                string userinfoResponseContent = await userinfoResponse.Content.ReadAsStringAsync();
+
+                await Nav.HidePopUp();
+                await UserSignedIn.Raise(userinfoResponse);
+            }
         }
     }
 }
