4795bf3fd441b83c39bcf0860b8f32b637967912
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-11-24 13:52:40 +0330

    Upgrade to Android 12, Use the updated Google APIs

--- Android/Google.cs
+++ Android/Google.cs
@@ -1,82 +1,86 @@
 ﻿namespace Zebble
 {
-    using Android.Gms.Auth.Api;
     using Android.Gms.Auth.Api.SignIn;
     using Android.Gms.Common.Apis;
     using System.Threading.Tasks;
     using Olive;
+    using System;
+    using Microsoft.Extensions.Logging;
+    using Android.App;
+    using Android.Content;
 
     public static partial class Google
     {
         const int SIGNIN_REQUEST_CODE = 9001;
-        static GoogleApiClient ApiClient;
+        static GoogleSignInClient Client;
+
+        static ILogger Logger => Log.For(typeof(Google));
 
         public static void Initialize()
         {
-            UIRuntime.OnActivityResult.Handle(args =>
-            {
-                if (args.Item1 == SIGNIN_REQUEST_CODE && args.Item2 == Android.App.Result.Ok)
-                {
-                    if(args.Item3 == null)
-                    {
-                        Log.For(typeof(Google)).Error("[Zebble.Google] => The Google Play Services are not installed on your device, please make sure to installed them");
-                        return;
-                    }
+            UIRuntime.OnActivityResult.Handle(HandleActivityResult);
+            Client = CreateClient(UIRuntime.CurrentActivity);
+        }
 
-                    var result = Auth.GoogleSignInApi.GetSignInResultFromIntent(args.Item3);
-                    if (result.IsSuccess)
-                    {
-                        var account = result.SignInAccount;
-                        UserSignedIn.Raise(new User
-                        {
-                            FamilyName = account.FamilyName,
-                            GivenName = account.GivenName,
-                            Id = account.Id,
-                            Name = account.DisplayName,
-                            Picture = account.PhotoUrl?.ToString(),
-                            Email = account.Email,
-                            Token = account.IdToken
-                        });
-                    }
-                }
-            });
-
-            var context = UIRuntime.CurrentActivity;
-            var serverClientIdStr = context.Resources.GetIdentifier("server_client_id", "string", context.PackageName);
-            if (serverClientIdStr == 0)
+        public static async Task SignIn()
+        {
+            try
             {
-                Log.For(typeof(Google)).Error("Google Client ID is not set on Android application. Please add server_client_id to the resource string file.");
-                return;
+                var signInAccount = await Client.SilentSignInAsync();
+
+                if (TryToUseSignInAccount(signInAccount)) return;
             }
+            catch (Exception ex) { Logger.Error(ex, "Faild to log in silently."); }
 
-            var clientId = context.GetString(serverClientIdStr);
-            var gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
+            InitiateSignIn();
+        }
+
+        static GoogleSignInClient CreateClient(Activity activity)
+        {
+            var serverClientIdStr = activity.Resources.GetIdentifier("server_client_id", "string", activity.PackageName);
+            if (serverClientIdStr == 0)
+                throw new ArgumentException("Google Client ID is not set on Android application. Please add server_client_id to the resource string file.");
+
+            var clientId = activity.GetString(serverClientIdStr);
+            var options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
                 .RequestEmail()
                 .RequestProfile()
                 .RequestIdToken(clientId)
                 .Build();
 
-            ApiClient = new GoogleApiClient.Builder(UIRuntime.CurrentActivity)
-                .AddApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .AddConnectionCallbacks(connectedCallback: bundle =>
-                 {
-                     if (bundle != null) Log.For(typeof(Google)).Debug("Google connected");
-                     else Log.For(typeof(Google)).Error("Google connection filed");
-                 }).Build();
-
-            ApiClient.Connect();
+            return GoogleSignIn.GetClient(activity, options);
         }
 
-        public static async Task SignIn()
+        static async Task HandleActivityResult(Tuple<int, Result, Intent> args)
         {
-            var signInIntent = Auth.GoogleSignInApi.GetSignInIntent(ApiClient);
+            if (args.Item1 != SIGNIN_REQUEST_CODE) return;
 
-            if (ApiClient != null && ApiClient.IsConnected)
-                await ApiClient.ClearDefaultAccountAndReconnect();
+            var signInAccount = await GoogleSignIn.GetSignedInAccountFromIntentAsync(args.Item3);
+            TryToUseSignInAccount(signInAccount);
+        }
 
+        static void InitiateSignIn()
+        {
+            var signInIntent = Client.SignInIntent;
             UIRuntime.CurrentActivity.StartActivityForResult(signInIntent, SIGNIN_REQUEST_CODE);
+        }
 
-            await Task.CompletedTask;
+        static bool TryToUseSignInAccount(GoogleSignInAccount account)
+        {
+            if (account == null) return false;
+
+            UserSignedIn.Raise(new User
+            {
+                FamilyName = account.FamilyName,
+                GivenName = account.GivenName,
+                Id = account.Id,
+                Name = account.DisplayName,
+                Picture = account.PhotoUrl?.ToString(),
+                Email = account.Email,
+                Token = account.IdToken
+            });
+
+            return true;
         }
     }
 }
